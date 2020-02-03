f83036e0ec6c8700eea5956515c8358971392d9f
hzhuangzhexiao <hzhuangzhexiao@corp.netease.com>
2020-02-03 16:17:18 +0800

    new mutate json cli tools

---
+++ mutate_json/mutate json命令行工具使用.md
@@ -0,0 +1,28 @@
+# 命令行工具使用
+
+基本说明
+
+$ python mutate_json.py -h
+usage: mutate_json.py [-h] -n NUMBER data
+
+positional arguments:
+  data                  a valid string json data
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n NUMBER, --number NUMBER
+                        return how many mutated jsons
+                        
+ 
+-n 参数指定返回的mutated json 个数
+
+使用demo
+$ python mutate_json.py -n 3 '{"network": {"cidr": "20.100.0.0/16", "name": "hzx-vpc-test1", "admin_state_up": true}}'
+
+['{"network": {"cidr": "5100.100.0.0/16", "name": "hzx-vpc-test1""hzx-vpc-test1""hzx-vpc-test1""hzx-vpc-test1""hzx-vpc-t
+est1""hzx-vpc-test1", : true}}', '{"network": {"c\x90\x06\x00\x00: "20.100.0.\x18/16", "name": "hzx-vpc-test1", "admin_s
+tate_up": true}}']
+
+每次都会随机返回3条变异后的json
+
+                       
--- mutate_json/mutate_json.py
+++ mutate_json/mutate_json.py
@@ -1,16 +1,18 @@
-from pyjfuzz.core.pjf_server import PJFServer
 from pyjfuzz.lib import PJFConfiguration
 from pyjfuzz.lib import PJFFactory
 from argparse import Namespace
 import json
 import argparse
 
-data = '{"NetworkTypes":["private","idc"],"VpcId":"233a5ef1-25c0-417c-8984-f684c99b9b9f","FlavorId":80,"ClusterName":"1ddb-ab-smoke-quick-check-czq","ClusterSizes":[1,2],"DDBVersion":"5.0","VpcSecurityGroups":["6e9796ee-c87e-46ec-a0fb-da511b214d87"],"SubnetIds":["663a1aa0-7f97-41fd-a613-6cdb08bbb340","2a490371-5f38-49e0-8ed7-cc8fbaf37173"],"TopAvailableZones":["cn-east-1b","cn-east-1a"]}'
+
+# data = '{"NetworkTypes":["private","idc"],"VpcId":"233a5ef1-25c0-417c-8984-f684c99b9b9f","FlavorId":80,"ClusterName":"1ddb-ab-smoke-quick-check-czq","ClusterSizes":[1,2],"DDBVersion":"5.0","VpcSecurityGroups":["6e9796ee-c87e-46ec-a0fb-da511b214d87"],"SubnetIds":["663a1aa0-7f97-41fd-a613-6cdb08bbb340","2a490371-5f38-49e0-8ed7-cc8fbaf37173"],"TopAvailableZones":["cn-east-1b","cn-east-1a"]}'
 
 
 def set_argu_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--number", required=False,
+
+    parser.add_argument('data', help='a valid string json data ')
+    parser.add_argument("-n", "--number", required=True,
                         help="return how many mutated jsons")
 
     arguments = parser.parse_args()
@@ -20,9 +22,9 @@ def set_argu_parser():
 args = set_argu_parser()
 
 
-def print_mutated_json(number=10):
+def print_mutated_json(number):
     config = PJFConfiguration(Namespace(
-        json=json.loads(data),
+        json=json.loads(args.data),
         level=6,
         strong_fuzz=True,
         nologo=True,
@@ -32,8 +34,11 @@ def print_mutated_json(number=10):
     # init the object factory used to fuzz (see documentation)
     factory = PJFFactory(config)
 
+    sum = []
     for i in range(1, number):
-        print factory.fuzzed
+        sum.append(factory.fuzzed)
+
+    print sum
 
 
 if __name__ == '__main__':
