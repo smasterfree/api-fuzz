1226bd04cb17ff916ab094361cd33a697b67051d
hzhuangzhexiao <hzhuangzhexiao@corp.netease.com>
2020-02-06 16:47:47 +0800

    mutate request data json

--- uuun.py
+++ uuun.py
@@ -30,21 +30,21 @@ def make_request(method, url, header, data):
 
 if __name__ == '__main__':
     context = uncurl_lib.parse_context(
-        '''curl  'http://10.182.2.253:8774/v2/9ac08939bf67465c88cd638107e0a6d6/os-tag-types/11/extra-specs' -X POST -H "X-Auth-Project-Id: admin" -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: d8499072f58f4e01815812974e81ce9d" -d '{"extra_specs": {"host_required": "no", "unique_on_host": "dsadsa"}}' ''')
+        '''
+        curl  'http://10.182.2.253:8774/v2/9ac08939bf67465c88cd638107e0a6d6/os-tag-types/11/extra-specs' -X POST -H "X-Auth-Project-Id: admin" -H "Content-Type: application/json" -H "Accept: application/json" -H "X-Auth-Token: 318a94c85ec840f9be71bad2af8b309b" -d '{"extra_specs": {"host_required": "no", "unique_on_host": "dsadsa"}}' 
+        ''')
 
     uncurl_url = context.url
     uncurl_method = context.method
     uncurl_data = context.data
     uncurl_header = context.headers
 
-    print uncurl_url
-    print uncurl_method
-    print uncurl_data
-    print uncurl_header
+    for i in range(1, 100):
+        fuzzed_json = get_mutated_json(str(uncurl_data))
 
-    print get_mutated_json(str(uncurl_data))
-
-    print make_request(method=uncurl_method,
-                       url=uncurl_url,
-                       data=uncurl_data,
-                       header=uncurl_header)
+        res_code = make_request(method=uncurl_method,
+                                url=uncurl_url,
+                                header=uncurl_header,
+                                data=fuzzed_json,
+                                )
+        print str(res_code) + "\t" + fuzzed_json
