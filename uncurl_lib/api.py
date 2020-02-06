1372f97f39e7c1449f26a0ff6083217b1e55b462
hzhuangzhexiao <hzhuangzhexiao@corp.netease.com>
2020-02-06 16:17:47 +0800

    update uncurl library

--- uncurl_lib/api.py
+++ uncurl_lib/api.py
@@ -1,57 +1,47 @@
 import argparse
-from collections import OrderedDict
 import json
-from six.moves import http_cookies as Cookie
+import re
 import shlex
+from collections import OrderedDict, namedtuple
+
+from six.moves import http_cookies as Cookie
 
 parser = argparse.ArgumentParser()
 parser.add_argument('command')
-parser.add_argument('url', nargs="?")
+parser.add_argument('url')
 parser.add_argument('-d', '--data')
 parser.add_argument('-b', '--data-binary', default=None)
-parser.add_argument('-i', default=None)
 parser.add_argument('-X', default='')
 parser.add_argument('-H', '--header', action='append', default=[])
 parser.add_argument('--compressed', action='store_true')
 parser.add_argument('--insecure', action='store_true')
 
+BASE_INDENT = " " * 4
 
-def parse(curl_command):
+ParsedContext = namedtuple('ParsedContext', ['method', 'url', 'data', 'headers', 'cookies', 'verify'])
+
+def parse_context(curl_command):
     method = "get"
 
     tokens = shlex.split(curl_command)
     parsed_args = parser.parse_args(tokens)
 
-    base_indent = " " * 4
-    data_token = ''
-    if parsed_args.X.lower() == 'post':
-        method = 'post'
     post_data = parsed_args.data or parsed_args.data_binary
-
-    if parsed_args.i is not None:
-        final_url = parsed_args.i
-    else:
-        final_url = parsed_args.url
-
     if post_data:
         method = 'post'
-        try:
-            post_data_json = json.loads(post_data)
-        except ValueError:
-            post_data_json = None
 
-        # If we found JSON and it is a dict, pull it apart. Otherwise, just leave as a string
-        if post_data_json and isinstance(post_data_json, dict):
-            post_data = dict_to_pretty_string(post_data_json)
-        else:
-            post_data = "'{}'".format(post_data)
-
-        data_token = '{}'.format(post_data)
+    if parsed_args.X:
+        method = parsed_args.X.lower()
 
     cookie_dict = OrderedDict()
     quoted_headers = OrderedDict()
+
     for curl_header in parsed_args.header:
-        header_key, header_value = curl_header.split(":", 1)
+        if curl_header.startswith(':'):
+            occurrence = [m.start() for m in re.finditer(':', curl_header)]
+            header_key, header_value = curl_header[:occurrence[1]], curl_header[occurrence[1] + 1:]
+        else:
+            header_key, header_value = curl_header.split(":", 1)
 
         if header_key.lower() == 'cookie':
             cookie = Cookie.SimpleCookie(header_value)
@@ -60,37 +50,44 @@ def parse(curl_command):
         else:
             quoted_headers[header_key] = header_value.strip()
 
-    result_string = """requests.{method}("{url}",
-                    {data_token}{headers_token},
-                    {cookies_token},{security_token}
-                    )""".format(
+    return ParsedContext(
         method=method,
-        url=final_url,
-        data_token=data_token,
-        headers_token="{}".format(dict_to_pretty_string(
-            quoted_headers)),
-        cookies_token="{}cookies={}".format(base_indent,
-                                            dict_to_pretty_string(cookie_dict)),
-        security_token="\n%sverify=False" % base_indent if parsed_args.insecure else ""
+        url=parsed_args.url,
+        data=post_data,
+        headers=quoted_headers,
+        cookies=cookie_dict,
+        verify=parsed_args.insecure
     )
 
-    result_dict = {"method": method, "url": final_url,
-                   "data_token": data_token,
-                   "headers_token": "{}".format(dict_to_pretty_string(
-                       quoted_headers)),
-                   "cookies_token": "{}cookies={}".format(base_indent,
-                                                          dict_to_pretty_string(
-                                                              cookie_dict)),
-                   "security_token": "\n%sverify=False" % base_indent if parsed_args.insecure else ""}
 
-    return result_string, result_dict
+def parse(curl_command):
+    parsed_context = parse_context(curl_command)
 
+    data_token = ''
+    if parsed_context.data:
+        data_token = '{}data=\'{}\',\n'.format(BASE_INDENT, parsed_context.data)
+
+    verify_token = ''
+    if parsed_context.verify:
+        verify_token = '\n{}verify=False'.format(BASE_INDENT)
+
+    formatter = {
+        'method': parsed_context.method,
+        'url': parsed_context.url,
+        'data_token': data_token,
+        'headers_token': "{}headers={}".format(BASE_INDENT, dict_to_pretty_string(parsed_context.headers)),
+        'cookies_token': "{}cookies={}".format(BASE_INDENT, dict_to_pretty_string(parsed_context.cookies)),
+        'security_token': verify_token
+    }
+
+    return """requests.{method}("{url}",
+{data_token}{headers_token},
+{cookies_token},{security_token}
+)""".format(**formatter)
 
 def dict_to_pretty_string(the_dict, indent=4):
     if not the_dict:
         return "{}"
 
-    # return ("\n" + " " * indent).join(
-    #     json.dumps(the_dict, sort_keys=True, indent=indent,
-    #                separators=(',', ': ')).splitlines())
-    return json.dumps(the_dict)
+    return ("\n" + " " * indent).join(
+        json.dumps(the_dict, sort_keys=True, indent=indent, separators=(',', ': ')).splitlines())
